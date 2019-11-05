16394dcf027d7cc528b5abd63564961b2479c670
Nick Gyongyosi <lazarus@Lazarus.local>
2019-11-05 14:23:03 -0700

    balanced classes

--- src/appleseed.py
+++ src/appleseed.py
@@ -8,31 +8,29 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
-from load_data import ImageData
-
-class LocalSeeds(object):
+class Appleseed(object):
     def __init__(self, mode):
-        # load in test and train arrays saved as npz file
-        if mode == 'RGBA':
-        self.data = play_data
-        
         # assign train and test sets from saved subsamples
-        self.x_train_play = play_data['arr_0']
-        self.y_train_play = play_data['arr_1']
-        self.x_test_play = play_data['arr_2']
-        self.y_test_play = play_data['arr_3']
+        self.mode = mode
 
-        # reshape to what Conv2D prefers
-        self.x_train_play = self.x_train_play.reshape(self.x_train_play.shape[3], self.x_train_play.shape[0], 
-                                            self.x_train_play.shape[1], self.x_train_play.shape[2])
-        self.x_test_play = self.x_test_play.reshape(self.x_test_play.shape[3], self.x_test_play.shape[0], 
-                                          self.x_test_play.shape[1], self.x_test_play.shape[2])
-        self.y_train_play = self.y_train_play.T
-        self.y_test_play = self.y_test_play.T
+        self.sample_data = np.load('data/{}/{}_balanced_data.npz'.format(self.mode,self.mode))
+
+        self.x_train_sample = self.sample_data['arr_0']
+        self.y_train_sample = self.sample_data['arr_1']
+        self.x_test_sample = self.sample_data['arr_2']
+        self.y_test_sample = self.sample_data['arr_3']
+
+        # reshape to what Conv2D prefers--X: [samples(~6k), 28, 28, 4], y: [(samples(~1.5k), classes(6)]
+        # self.x_train_sample = self.x_train_sample.reshape(self.x_train_sample.shape[3], self.x_train_sample.shape[0], 
+        #                                     self.x_train_sample.shape[1], self.x_train_sample.shape[2])
+        # self.x_test_sample = self.x_test_sample.reshape(self.x_test_sample.shape[3], self.x_test_sample.shape[0], 
+        #                                   self.x_test_sample.shape[1], self.x_test_sample.shape[2])
+        # self.y_train_sample = self.y_train_sample.T
+        # self.y_test_sample = self.y_test_sample.T
 
         # .predict() doesn't like uint8
-        self.x_train_play = self.x_train_play.astype('float32') / 255. # data was uint8 [0-255]
-        self.x_test_play = self.x_test_play.astype('float32') / 255. # data was uint8 [0-255]
+        self.x_train_sample = self.x_train_sample.astype('float32') / 255. # data was uint8 [0-255]
+        self.x_test_sample = self.x_test_sample.astype('float32') / 255. # data was uint8 [0-255]
 
     def define_model(self, nb_filters, kernel_size, input_shape, pool_size):
         model = Sequential()
@@ -71,35 +69,51 @@ class LocalSeeds(object):
         self.model = model
 
     def fit_model(self):
-        self.model.fit(self.x_train_play, self.y_train_play, batch_size=batch_size, epochs=nb_epoch,
-        verbose=1, validation_data=(self.x_test_play, self.y_test_play))
+        self.model.fit(self.x_train_sample, 
+                       self.y_train_sample,
+                       batch_size=batch_size, 
+                       epochs=nb_epoch,
+                       verbose=1, 
+                       validation_data=(self.x_test_sample, self.y_test_sample))
     
     def evaluate_model(self):
-        score = self.model.evaluate(self.x_test_play, self.y_test_play, verbose=0)
-        print('Test score:', score[0])
-        print('Test accuracy:', score[1]) 
+        score = self.model.evaluate(self.x_test_sample, self.y_test_sample, verbose=0)
+        print('{} Test score:'.format(self.mode), score[0])
+        print('{} Test accuracy:'.format(self.mode), score[1]) 
+    
+    def check(self):
+        self.pred_probs = self.model.predict(self.x_test_sample)
+        print('Probabilities: {}'.format(self.pred_probs))
+        self.guesses = np.argmax(self.model.predict(self.x_test_sample), axis=1)
+        print('Guesses: {}'.format(self.guesses))
+        self.actual = np.argmax(self.y_test_sample, axis=1)
 
 if __name__ == "__main__":
-
     print('Creating class')
-    data = np.load('data/play_data.npz')
-    seeds = LocalSeeds(play_data)
+    apples = Appleseed(mode='RGB')
+    print(apples.x_train_sample.shape)
+    print(apples.y_train_sample.shape)
+    print(apples.x_test_sample.shape)
+    print(apples.y_test_sample.shape)
 
     print("Initializing parameters")
     batch_size = 10  # number of training samples used at a time to update the weights
     nb_classes = 6   # number of output possibilites: [0 - 5]
     nb_epoch = 10   # number of passes through the entire train dataset before weights "final"
     img_rows, img_cols = 28, 28  # the size of the NAIP images
-    input_shape = (img_rows, img_cols, 4)  # 4 channel image input (RGBA) 
+    input_shape = (img_rows, img_cols, len(apples.mode))  # 28x28x3 or 4(RGB/RGBA) 
     nb_filters = 8 # number of convolutional filters to use
     pool_size = (2, 2) # pooling decreases image size, reduces computation, adds translational invariance
     kernel_size = (4, 4) # convolutional kernel size, slides over image to learn features
 
     print('Building model')
-    seeds.define_model(nb_filters, kernel_size, input_shape, pool_size)
+    apples.define_model(nb_filters, kernel_size, input_shape, pool_size)
 
     print('Fitting model')
-    seeds.fit_model()
+    apples.fit_model()
 
     print("How'd we do?")
-    seeds.evaluate_model()
+    apples.evaluate_model()
+
+    print("What did it guess??")
+    apples.check()
--- src/load_data.py
+++ src/load_data.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class ImageData(object):
-    def __init__(self, mat, sample_size=1000, mode='RGBA'):
+    def __init__(self, mat, sample_size=6000, mode='RGBA'):
         # mode = 'RGBA' or 'RGB'
 
         # .mat loads in as a dictionary
@@ -29,95 +29,12 @@ class ImageData(object):
         # train and test sets
         self.y_train_cl = self.class_lister(self.y_train)
         self.y_test_cl = self.class_lister(self.y_test)
-
-    def save_data(self):
-        if self.mode == 'RGBA':
-            # create a local subset to load from when running model sript
-            np.savez('data/RGBA/rgba_data', self.x_train, 
-                                            self.y_train, 
-                                            self.x_test, 
-                                            self.y_test)
-        elif self.mode == 'RGB':
-            # create a local subset to load from when running model sript
-            np.savez('data/RGB/rgb_data', self.x_train, 
-                                           self.y_train, 
-                                           self.x_test, 
-                                           self.y_test)
-
-    def save_train_png(self):
-        # Saves train set folders with images you can look at to check out the data
-        
-        b = 0
-        bl = 0
-        t = 0
-        g = 0
-        r = 0
-        w = 0
-
-        for i in range(len(self.y_train_cl)):
-            data = self.x_train[:,:,:,i]
-            img = Image.fromarray(data,self.mode)
-        
-            if self.y_train_cl[i] == 0 and b < self.sample_size:
-                img.save("data/{}/x_train/0_building/building{}.png".format(self.mode,b))
-                b +=1
-            elif self.y_train_cl[i] == 1 and bl < self.sample_size:
-                img.save("data/{}/x_train/1_barren_land/barren{}.png".format(self.mode,bl))
-                bl+=1
-            elif self.y_train_cl[i] == 2 and t < self.sample_size:
-                img.save("data/{}/x_train/2_tree/tree{}.png".format(self.mode,t))
-                t+=1
-            elif self.y_train_cl[i] == 3 and g < self.sample_size:
-                img.save("data/{}/x_train/3_grassland/grassland{}.png".format(self.mode,g))
-                g+=1
-            elif self.y_train_cl[i] == 4 and r < self.sample_size:
-                img.save("data/{}/x_train/4_road/road{}.png".format(self.mode,r))
-                r+=1
-            elif self.y_train_cl[i] == 5 and w < self.sample_size:
-                img.save("data/{}/x_train/5_water/water{}.png".format(self.mode,w))
-                w+=1
-            else:
-                pass
-
-    def save_test_png(self):
-        # Saves test set directories with images you can look at to check out the data
-        b = 0
-        bl = 0
-        t = 0
-        g = 0
-        r = 0
-        w = 0
-
-        for i in range(len(self.y_test_cl)):
-            data = self.x_test[:,:,:,i]
-            img = Image.fromarray(data,self.mode)
-        
-            if self.y_test_cl[i] == 0 and b < self.test_sample_size:
-                img.save("data/{}/x_test/0_building/building{}.png".format(self.mode,b))
-                b +=1
-            elif self.y_test_cl[i] == 1 and bl < self.test_sample_size:
-                img.save("data/{}/x_test/1_barren_land/barren{}.png".format(self.mode,bl))
-                bl+=1
-            elif self.y_test_cl[i] == 2 and t < self.test_sample_size:
-                img.save("data/{}/x_test/2_tree/tree{}.png".format(self.mode,t))
-                t+=1
-            elif self.y_test_cl[i] == 3 and g < self.test_sample_size:
-                img.save("data/{}/x_test/3_grassland/grassland{}.png".format(self.mode,g))
-                g+=1
-            elif self.y_test_cl[i] == 4 and r < self.test_sample_size:
-                img.save("data/{}/x_test/4_road/road{}.png".format(self.mode,r))
-                r+=1
-            elif self.y_test_cl[i] == 5 and w < self.test_sample_size:
-                img.save("data/{}/x_test/5_water/water{}.png".format(self.mode,w))
-                w+=1
-            else:
-                pass
-
+    
     def class_lister(self, y_array):
         '''
         Input: y_train, y_test
         Returns: list of classifications (0-5) for each array in
-                 the corresponding x array
+                    the corresponding x array
         0 = building
         1 = barren land
         2 = trees
@@ -134,19 +51,136 @@ class ImageData(object):
             i+=1
         return class_list
 
+    def save_train_png(self):
+        # Saves train set folders with images you can look at to check out the data,
+        # and a number of arrays from each class equal to self.sample_size parameters 
+        b = 0
+        bl = 0
+        t = 0
+        g = 0
+        r = 0
+        w = 0
+        x_train_balanced = []
+        y_train_cl_idx = []
+
+        for i in range(len(self.y_train_cl)):
+            data = self.x_train[:,:,:,i]
+            img = Image.fromarray(data,self.mode)
+        
+            if self.y_train_cl[i] == 0 and b < (self.sample_size / 6):
+                img.save("data/{}/x_train/0_building/building{}_{}.png".format(self.mode,b,i))
+                x_train_balanced.append(data)
+                y_train_cl_idx.append(i)
+                b +=1
+            elif self.y_train_cl[i] == 1 and bl < (self.sample_size / 6):
+                img.save("data/{}/x_train/1_barren_land/barren{}_{}.png".format(self.mode,bl,i))
+                x_train_balanced.append(data)
+                y_train_cl_idx.append(i)
+                bl+=1
+            elif self.y_train_cl[i] == 2 and t < (self.sample_size / 6):
+                img.save("data/{}/x_train/2_tree/tree{}_{}.png".format(self.mode,t,i))
+                x_train_balanced.append(data)
+                y_train_cl_idx.append(i)
+                t+=1
+            elif self.y_train_cl[i] == 3 and g < (self.sample_size / 6):
+                img.save("data/{}/x_train/3_grassland/grassland{}_{}.png".format(self.mode,g,i))
+                x_train_balanced.append(data)
+                y_train_cl_idx.append(i)
+                g+=1
+            elif self.y_train_cl[i] == 4 and r < (self.sample_size / 6):
+                img.save("data/{}/x_train/4_road/road{}_{}.png".format(self.mode,r,i))
+                x_train_balanced.append(data)
+                y_train_cl_idx.append(i)
+                r+=1
+            elif self.y_train_cl[i] == 5 and w < (self.sample_size / 6):
+                img.save("data/{}/x_train/5_water/water{}_{}.png".format(self.mode,w,i))
+                x_train_balanced.append(data)
+                y_train_cl_idx.append(i)
+                w+=1
+            else:
+                pass
+        
+        self.x_train_balanced = np.asarray(x_train_balanced)
+        self.y_train_balanced = self.y_train.T[y_train_cl_idx]
+
+    def save_test_png(self):
+        # Saves test set directories with images you can look at to check out the data
+        b = 0
+        bl = 0
+        t = 0
+        g = 0
+        r = 0
+        w = 0
+        x_test_balanced = []
+        y_test_cl_idx = []
+
+        for i in range(len(self.y_test_cl)):
+            data = self.x_test[:,:,:,i]
+            img = Image.fromarray(data,self.mode)
+            
+            if self.y_test_cl[i] == 0 and b < (self.test_sample_size / 6):
+                img.save("data/{}/x_test/0_building/building{}_{}.png".format(self.mode,b,i))
+                x_test_balanced.append(data)
+                y_test_cl_idx.append(i)
+                b +=1
+            elif self.y_test_cl[i] == 1 and bl < (self.test_sample_size / 6):
+                img.save("data/{}/x_test/1_barren_land/barren{}_{}.png".format(self.mode,bl,i))
+                x_test_balanced.append(data)
+                y_test_cl_idx.append(i)
+                bl+=1
+            elif self.y_test_cl[i] == 2 and t < (self.test_sample_size / 6):
+                img.save("data/{}/x_test/2_tree/tree{}_{}.png".format(self.mode,t,i))
+                x_test_balanced.append(data)
+                y_test_cl_idx.append(i)
+                t+=1
+            elif self.y_test_cl[i] == 3 and g < (self.test_sample_size / 6):
+                img.save("data/{}/x_test/3_grassland/grassland{}_{}.png".format(self.mode,g,i))
+                x_test_balanced.append(data)
+                y_test_cl_idx.append(i)
+                g+=1
+            elif self.y_test_cl[i] == 4 and r < (self.test_sample_size / 6):
+                img.save("data/{}/x_test/4_road/road{}_{}.png".format(self.mode,r,i))
+                x_test_balanced.append(data)
+                y_test_cl_idx.append(i)
+                r+=1
+            elif self.y_test_cl[i] == 5 and w < (self.test_sample_size / 6):
+                img.save("data/{}/x_test/5_water/water{}_{}.png".format(self.mode,w,i))
+                x_test_balanced.append(data)
+                y_test_cl_idx.append(i)
+                w+=1
+            else:
+                pass
+        
+        self.x_test_balanced = np.asarray(x_test_balanced)
+        self.y_test_balanced = self.y_test.T[y_test_cl_idx]
+
+    def save_data(self):
+        if self.mode == 'RGBA':
+            # create a local subset to load from when running model sript
+            np.savez('data/RGBA/RGBA_balanced_data', self.x_train_balanced, 
+                                                     self.y_train_balanced,
+                                                     self.x_test_balanced, 
+                                                     self.y_test_balanced)
+        elif self.mode == 'RGB':
+            # create a local subset to load from when running model sript
+            np.savez('data/RGB/RGB_balanced_data', self.x_train_balanced, 
+                                                   self.y_train_balanced,
+                                                   self.x_test_balanced, 
+                                                   self.y_test_balanced)
+
 if __name__ == "__main__":
     # Be sure to set 'mode' parameter before running script
     mat = scipy.io.loadmat('data/sat-6-full.mat')
-    data = ImageData(mat, mode='RGB')
-
-    print("Saving arrays...")
-    data.save_data()
+    data = ImageData(mat, mode='RGBA')
     
-    print("Saving training images...")
+    print("Saving {} training images...".format(data.mode))
     data.save_train_png()
 
-    print("Saving testing images...")
+    print("Saving {} testing images...".format(data.mode))
     data.save_test_png()
+
+    print("Saving {} arrays...".format(data.mode))
+    data.save_data()
 
     print("Done!")
 
